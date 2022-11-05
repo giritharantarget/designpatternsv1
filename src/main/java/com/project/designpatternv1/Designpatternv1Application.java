2d01c0d1ebf017a90642e4d0d6e27dd53818cc65
Giritharan Balasubramaniam <giri@Giritharans-MacBook-Pro.local>
2022-11-05 14:30:44 -0500

    first commit

---
+++ src/main/java/com/project/designpatternv1/Designpatternv1Application.java
@@ -0,0 +1,13 @@
+package com.project.designpatternv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Designpatternv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Designpatternv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/designpatternv1/Designpatternv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.designpatternv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Designpatternv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
